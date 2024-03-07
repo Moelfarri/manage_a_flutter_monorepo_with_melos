afa7a5963562a3e742bd7ed9cbdd41c760ff59e2
EliasElfarri <elias.elfarri@inline-test.no>
2024-03-07 17:17:26 +0100

    first commit

---
+++ apps/text_resizer_app/android/app/src/main/kotlin/com/example/text_resizer_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.text_resizer_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ packages/company_business_logic_package/android/app/src/main/kotlin/com/example/company_business_logic_package/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.company_business_logic_package
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
